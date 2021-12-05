9f9aa68ba71e1b03aed1f632052854ed355af184
Emaleth <Emaleth@protonmail.com>
2021-12-05 13:55:26 +0100

    something

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 set fish_greeting 
+
 set -gx EDITOR nvim
+
 set NIX_LINK $HOME/.nix-profile
+
 starship init fish | source
 
 set PATH "$HOME/.cargo/bin:$PATH"
+
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
